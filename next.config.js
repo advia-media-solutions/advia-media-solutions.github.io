e1f7b36bfa43aa88c177ff1c92fd8bf15d570102
Daniel B. <dxffffff@protonmail.com>
2026-05-19 11:03:25 +0200

    feat(config): add CORS headers configuration in next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   compress: true,
+  async headers() {
+    return [
+      {
+        source: "/.well-known/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET, HEAD, OPTIONS" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+          { key: "Access-Control-Max-Age", value: "86400" },
+        ],
+      },
+    ];
+  },
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
